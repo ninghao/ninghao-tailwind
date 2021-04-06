8d01d281119ef87f1adde3a33c5dc77102e693cb
wanghao <wanghao@ninghao.net>
2021-04-06 20:22:53 +0800

    动画：Animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+        'spin-slow-reverse': 'spin 5s linear infinite reverse',
+      }
+    },
   },
   variants: {
     extend: {},
